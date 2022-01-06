d04d7a4d9b414cd6717a119756d7ebaf6ec2f3ef
Paweł Lasota <pawel.lasota@appwise.pl>
2022-01-06 22:09:53 +0100

    Icons / improve search / styles

--- components/footer.php
+++ components/footer.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-12 footer__credits">
                 <div class="footer__social-icons">
-                    <a href="https://web.facebook.com/"><i class="icon icon-facebook"></i></a>
+                    <a href="https://www.facebook.com/koszalindladzieci/"><i class="icon icon-facebook"></i></a>
                     <a href="https://www.instagram.com/"><i class="icon icon-instagram"></i></a>
                 </div> 
                 <p class="mb-2 text-white">© <?php echo date('Y'); ?> <?php echo get_bloginfo(); ?>.pl</p>       
