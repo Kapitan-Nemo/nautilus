e647cac56120c0ece052b7844309855543273c24
Paweł Lasota <pawel.lasota@appwise.pl>
2022-01-11 22:36:22 +0100

    Event cards links

--- templates/homepage/event-cards.php
+++ templates/homepage/event-cards.php
@@ -4,7 +4,7 @@
         <div class="row">
           <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xxl-3 mb-5 mb-xl-0">
               <div class="event-cards__card">
-                      <a href="">
+                      <a href="<?php echo get_site_url(); ?>/wiosna">
                         <img width="400" height="300" class="event-cards__img img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/png/wiosna.png" alt="Wiosna w koszalinie" title="Wiosna w koszalinie" />
                       </a>  
                   <div class="event-cards__body">
@@ -16,7 +16,7 @@
           </div>
           <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xxl-3 mb-5 mb-xl-0">
               <div class="event-cards__card">
-                      <a href="">
+                      <a href="<?php echo get_site_url(); ?>/lato">
                         <img width="400" height="300" class="event-cards__img img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/png/lato.png" alt="Lato w koszalinie" title="Lato w koszalinie" />
                       </a>  
                   <div class="event-cards__body">
@@ -28,7 +28,7 @@
           </div>
           <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xxl-3 mb-5 mb-xl-0">
               <div class="event-cards__card">
-                      <a href="">
+                      <a href="<?php echo get_site_url(); ?>/jesien">
                         <img width="400" height="300" class="event-cards__img img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/png/jesien.png" alt="Jesien w koszalinie" title="Jesien w koszalinie" />
                       </a>  
                   <div class="event-cards__body">
@@ -40,7 +40,7 @@
           </div>
           <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xxl-3 mb-5 mb-xl-0">
               <div class="event-cards__card">
-                      <a href="">
+                      <a href="<?php echo get_site_url(); ?>/zima">
                         <img width="400" height="300" class="event-cards__img img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/png/zima.png" alt="Zima w koszalinie" title="Zima w koszalinie" />
                       </a>  
                   <div  class="event-cards__body">
